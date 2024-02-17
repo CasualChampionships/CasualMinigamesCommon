11beab783f6e35847e56d3ccd66ef8e4608508c2
senseiwells <wellsm488@gmail.com>
2024-02-17 16:48:12 +0000

    Fix glowing background HHMMSS

--- src/main/kotlin/net/casual/championships/common/ui/GlowingBossBar.kt
+++ src/main/kotlin/net/casual/championships/common/ui/GlowingBossBar.kt
@@ -3,6 +3,7 @@ package net.casual.championships.common.ui
 import net.casual.arcade.gui.bossbar.TimerBossBar
 import net.casual.arcade.utils.BossbarUtils
 import net.casual.arcade.utils.TimeUtils.formatHHMMSS
+import net.casual.arcade.utils.TimeUtils.formatMMSS
 import net.casual.championships.common.util.CommonComponents
 import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
@@ -10,7 +11,7 @@ import net.minecraft.world.BossEvent
 
 class GlowingBossBar: TimerBossBar() {
     override fun getTitle(player: ServerPlayer): Component {
-        return CommonComponents.GLOWING_BACKGROUNDED.generate(this.getRemainingDuration().formatHHMMSS())
+        return CommonComponents.GLOWING_BACKGROUNDED.generate(this.getRemainingDuration().formatMMSS())
     }
 
     override fun getProgress(player: ServerPlayer): Float {
